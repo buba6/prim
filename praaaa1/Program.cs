9a4a36e6f828e3a4ab92006efab3b72f1d239132
chuyano <spawner.astral@hotmail.com>
2015-02-24 15:39:50 -0600

    base del proyecto

---
+++ praaaa1/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: buba
+ * Date: 24/02/2015
+ * Time: 03:37 p.m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace praaaa1
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
